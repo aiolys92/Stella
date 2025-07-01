2cbc394af3474ccd95a020b779ba3a18176112ac
aiolys92 <denis.villermet@gmail.com>
2025-07-01 20:32:04 +0200

    Add files via upload

--- site.js
+++ site.js
@@ -1,4 +1,22 @@
 
+function addInventaireRow(type) {
+  const tableId = type === 'pieces' ? 'inventaireTablePieces' : 'inventaireTableGemmes';
+  const table = document.getElementById(tableId);
+  const objList = type === 'pieces' ? monnaies.pieces : monnaies.gemmes;
+  const row = table.insertRow(-1);
+  const options = Object.keys(objList)
+    .map(n => `<option value="${n}">${n}</option>`)
+    .join('');
+  row.innerHTML = `
+    <td><select onchange="updateInventaire(event)">${options}</select></td>
+    <td><input type='number' value='0' class='qte' oninput='updateInventaire(event)'></td>
+    <td class='valeur'>0</td>
+    <td class='total'>0</td>
+  `;
+}
+
+
+
 function updateInventaire(evt) {
   let totalPM = 0;
   ['inventaireTablePieces', 'inventaireTableGemmes'].forEach(id => {
